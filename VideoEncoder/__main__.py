1f862ae7d29071e9271f97aef306d0bdc0109342
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-13 09:57:18 +0530

    Update __main__.py

--- VideoEncoder/__main__.py
+++ VideoEncoder/__main__.py
@@ -1,20 +1,4 @@
 
-# VideoEncoder - a telegram bot for compressing/encoding videos in h264 format.
-# Copyright (c) 2021 WeebTime/VideoEncoder
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import logging
 from os import supports_dir_fd
 
@@ -25,7 +9,7 @@ from . import app
 
 async def main():
     await app.start()
-    print(f'[Started]: @{{(await app.get_me()).username}}')
+    print(f'[Started]: @{(await app.get_me()).username}')
     await idle()
     await app.stop()
 
